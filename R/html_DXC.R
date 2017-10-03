43c17ff2729662b5476bc7aaf2c96b2f915d65fa
Marc-Antoine Giuliani <mgiuliani3@csc.com>
2017-10-03 15:30:53 +0200

    Adds basic documentation

--- R/html_DXC.R
+++ R/html_DXC.R
@@ -1,11 +1,16 @@
-
+#' Generate a DXC markdown document
+#'
+#' @return A databook.
 html_DXC <- function() {
 
-header <- system.file("rmarkdown/templates/dxc_template/skeleton/header.html", package="dxcrmd")
-style <- system.file("rmarkdown/templates/dxc_template/skeleton/styles.css", package="dxcrmd")
+header <- system.file("rmarkdown/templates/dxc_template/skeleton/header.html",
+                      package="dxcrmd")
+
+style <- system.file("rmarkdown/templates/dxc_template/skeleton/styles.css",
+                     package="dxcrmd")
 
   # call the base html_document function
-  rmarkdown::html_document(title = " ",
+  rmarkdown::html_document(title = " ", # purpose ?
   	                        toc = TRUE,
                             toc_float = TRUE,
                             df_print = "paged",
